84630fed8c820a81c460f8c6b7ec3ad8ba672f52
Archit Shinde <archits581@gmail.com>
2020-11-12 19:55:54 +0530

    Change database settings

--- HomeSpace/HomeSpace/settings.py
+++ HomeSpace/HomeSpace/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'HomeSpace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HomeSpace',
+        'USER': 'postgres',
+        'PASSWORD': 'Black@@Hawk581/*postgresql',
+        'HOST': 'localhost',
     }
 }
 
