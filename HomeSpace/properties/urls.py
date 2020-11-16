c8c011c71d0fa63ba11f74d3230b310bbece07a1
Archit Shinde <archits581@gmail.com>
2020-11-16 21:24:39 +0530

    Add multiple image upload functionality

--- HomeSpace/properties/urls.py
+++ HomeSpace/properties/urls.py
@@ -7,5 +7,6 @@ app_name = 'properties'
 urlpatterns = [
     path('add/', views.createPropertyView, name='add'),
     path('', views.homePage, name='home'),
-    path('add_photos/', views.add_property_photos, name='add-photos'),
+    path('add_photos/<int:pk>', views.add_property_photos, name='add-photos'),
+    path('add_photos/image_upload/<int:pk>', views.add_property_photos_util, name='add-photos-util'),
 ]
--- HomeSpace/properties/views.py
+++ HomeSpace/properties/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PropertyDescriptionForm
-from .models import PropertyDescription
+from .models import PropertyDescription, PropertyImage
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
 
@@ -17,7 +18,9 @@ def createPropertyView(request):
             owner = get_object_or_404(User, pk=request.user.pk)
             property_object = form.save(commit=False)
             property_object.user=owner
-            return redirect('properties:all')
+            property_object.save()
+            pk = property_object.pk
+            return HttpResponseRedirect(reverse('properties:add-photos', args=(pk,)))
     else:
         form = PropertyDescriptionForm()
     return render(request, 'properties/add.html', {'form': form})
@@ -25,12 +28,16 @@ def createPropertyView(request):
 def homePage(request):
     return render(request, 'base.html', {})
 
-def add_property_photos(request):
-    return render(request, 'properties/add_photos.html', {})
+def add_property_photos(request, pk):
+    return render(request, 'properties/add_photos.html', {'pk': pk})
 
-def add_property_photos_util(request):
+def add_property_photos_util(request, pk):
     if request.method == 'POST':
         image_file = request.FILES.get('file')
-
+        try:
+            property_object = PropertyDescription.objects.get(pk=pk)
+            PropertyImage.objects.create(image=image_file, property=property_object)
+        except PropertyDescription.DoesNotExist:
+            raise Http404('Property does not exist')
         return HttpResponse('')
     return JsonResponse({'post': 'false'})
